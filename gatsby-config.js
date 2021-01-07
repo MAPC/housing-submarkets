c50c190c8a6e6d8492a5f2e25c0804026b1428ec
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-01-07 11:18:53 -0500

    Replace Gatsby references for MAPC, remove extraneous config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'MAPC Housing Submarkets',
+    description: 'Website for MAPC\'s research into housing submarkets in metropolitan Boston',
+    author: '@mapcmetroboston',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +18,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `MAPC Housing Submarkets`,
+        short_name: `Housing Submarkets`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/MAPC_logo.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
