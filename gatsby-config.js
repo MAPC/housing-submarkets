62dfaa2aba18c499e7365d706d5ab589d9f0da24
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-01-07 12:49:51 -0500

    Set up eslint and run auto-fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,26 +5,26 @@ module.exports = {
     author: '@mapcmetroboston',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `MAPC Housing Submarkets`,
-        short_name: `Housing Submarkets`,
-        start_url: `/`,
-        display: `minimal-ui`,
-        icon: `src/images/MAPC_logo.png`, // This path is relative to the root of the site.
+        name: 'MAPC Housing Submarkets',
+        short_name: 'Housing Submarkets',
+        start_url: '/',
+        display: 'minimal-ui',
+        icon: 'src/images/MAPC_logo.png', // This path is relative to the root of the site.
       },
     },
   ],
-}
+};
