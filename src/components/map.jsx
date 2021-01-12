8ee6849a5d25bc868417804c08d79ecd6e79faba
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-01-12 16:23:02 -0500

    Style tracts with original colors

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Source, Layer } from 'react-map-gl';
 
-const Map = () => {
+const Map = ({ data }) => {
   const [viewport, setViewport] = useState({
     latitude: 42.37,
     longitude: -70.944,
     zoom: 8.4,
   });
+
+  const colors = ['#B1DE68', '#F7695F', '#B0AAD1', '#7DCBBB', '#FFFFA4', '#6EA1C9', '#FBA550'];
+  const choropleth = ['match', ['get', 'ct10_id']];
+
+  data.forEach((row) => {
+    choropleth.push(row.ct10_id, colors[+row.class - 1]);
+  });
+  choropleth.push('grey');
+
   return (
     <ReactMapGL
       {...viewport}
@@ -15,7 +24,11 @@ const Map = () => {
       onViewportChange={(viewport) => setViewport(viewport)}
       mapboxApiAccessToken={process.env.GATSBY_MAPBOX_ACCESS_TOKEN}
       mapStyle="mapbox://styles/ihill/ckjn5vkva2jbv19oxvi39hc66"
-    />
+    >
+      <Source id="2010 Census Tracts" type="vector" url="mapbox://ihill.aw7gvvhk">
+        <Layer type="fill" id="tract-layer" source-layer="Tracts-2jsl06" paint={{ 'fill-color': choropleth, 'fill-outline-color': '#231F20' }} />
+      </Source>
+    </ReactMapGL>
   );
 };
 
--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import { Helmet } from 'react-helmet';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Map from '../components/map';
 import '../styles/app.scss';
 
-const MapPage = () => (
+const MapPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <Helmet>
       <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.0.1/mapbox-gl.css" rel="stylesheet" />
     </Helmet>
     <h1>MAPC Housing Submarkets</h1>
-    <Map />
+    <Map data={data.allDataCsv.nodes} />
   </Layout>
 );
 
 export default MapPage;
+export const data = graphql`
+  query {
+    allDataCsv {
+      nodes {
+        ct10_id
+        class
+      }
+    }
+  }
+`;
