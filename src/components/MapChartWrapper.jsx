cf8803cae99a0765d820a7cdcb8b461f77bb1d61
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-01-14 10:30:46 -0500

    Add choropleth for MHI

---
+++ src/components/MapChartWrapper.jsx
@@ -0,0 +1,75 @@
+import React, { useReducer } from 'react';
+import ReactMapGL, { Source, Layer } from 'react-map-gl';
+import { VegaLite } from 'react-vega';
+
+const MapChartWrapper = ({ data }) => {
+  const initialState = {
+    viewport: {
+      latitude: 42.365386297918825,
+      longitude: -71.08346183618282,
+      zoom: 8.4,
+    },
+  };
+
+  const colors = ['#f2f0f7', '#dadaeb', '#bcbddc', '#9e9ac8', '#807dba', '#6a51a3', '#4a1486'];
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'setViewport':
+        return { ...state, viewport: action.viewport };
+      default:
+        return { state };
+    }
+  }
+
+  function choroplethColor(value) {
+    if (value <= 35000) {
+      return colors[6];
+    } if (value <= 50000) {
+      return colors[5];
+    } if (value <= 75000) {
+      return colors[4];
+    } if (value <= 100000) {
+      return colors[3];
+    } if (value <= 150000) {
+      return colors[2];
+    } if (value <= 200000) {
+      return colors[1];
+    } if (value <= 250000) {
+      return colors[0];
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const choropleth = ['match', ['get', 'ct10_id']];
+  data.forEach((row) => {
+    choropleth.push(row.ct10_id, +row.mhi ? choroplethColor(+row.mhi) : 'rgba(0, 0, 0, 0)');
+  });
+  choropleth.push('rgb(255, 255, 255)');
+
+  return (
+    <div>
+      <ReactMapGL
+        {...state.viewport}
+        width="600px"
+        height="500px"
+        onViewportChange={(viewport) => dispatch({ type: 'setViewport', viewport })}
+        mapboxApiAccessToken="pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg"
+        mapStyle="mapbox://styles/ihill/ckjn5vkva2jbv19oxvi39hc66"
+      >
+        <Source id="2010 Census Tracts" type="vector" url="mapbox://ihill.aw7gvvhk">
+          <Layer
+            type="fill"
+            id="Tract choropleth"
+            source="2010 Census Tracts"
+            source-layer="Tracts-2jsl06"
+            paint={{ 'fill-color': choropleth, 'fill-outline-color': '#231F20' }}
+          />
+        </Source>
+      </ReactMapGL>
+    </div>
+  );
+};
+
+export default MapChartWrapper;
---
+++ src/pages/ScrollChart.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Helmet } from 'react-helmet';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import MapChartWrapper from '../components/MapChartWrapper';
+import '../styles/app.scss';
+
+const ScrollChart = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <Helmet>
+      <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.13.0/mapbox-gl.css" rel="stylesheet" />
+    </Helmet>
+    <h1>MAPC Housing Submarkets</h1>
+    <MapChartWrapper data={data.allDataCsv.nodes} />
+  </Layout>
+);
+
+export default ScrollChart;
+export const data = graphql`
+  query {
+    allDataCsv {
+      nodes {
+        ct10_id
+        class
+        mhi
+      }
+    }
+  }
+`;
