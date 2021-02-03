aad31cbfec53556e2d0e7b4275c13f4372ba37ed
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-02-03 09:10:15 -0500

    Toggle overlay and summary box on focus/blur

--- src/components/landing-page/GridItem.tsx
+++ src/components/landing-page/GridItem.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import { css, jsx } from "@emotion/react";
 import { fonts, themeColors, submarketColors } from '../../utils/theme';
@@ -16,31 +16,6 @@ const imageStyle = css`
   height: 35rem;
 `;
 
-const summaryStyle = css`
-  color: ${themeColors.white};
-  /* display: none; */
-  margin: 2.5rem 3rem;
-  padding: 2rem 4rem;
-  position: absolute;
-  top: 10rem;
-`;
-
-const overlayStyle = css`
-  background-color: rgba(255, 255, 255, 0.15);
-  height: 35rem;
-  position: absolute;
-  top: 0;
-  width: 100%;
-
-  :hover {
-    background-color: rgba(255, 255, 255, 0);
-
-    & + p {
-      display: inline;
-    }
-  }
-`;
-
 const h3Style = css`
   color: ${themeColors.white};
   font-family: ${fonts.calibre};
@@ -53,31 +28,53 @@ const h3Style = css`
   z-index: 10;
 `;
 
-const GridItem: React.FC<GridItemProps> = ({ submarket, image, ...props }) => (
-  <Link
-    {...props}
-    to={`/submarkets/${submarket}`}
-  >
-    <div css={css`
-      ${imageStyle}
-      background-image: url(${image});
-    `}/>
-    <div css={overlayStyle} />
-    <h3 css={css`
-      ${h3Style}
-      background-color: ${submarketColors[submarket]};
-    `}>
-      {`Submarket ${submarket}`}
-    </h3>
-    <p
-      data-role="summary"
-      css={css`
-        ${summaryStyle}
+const GridItem: React.FC<GridItemProps> = ({ submarket, image, ...props }) => {
+  const [isActive, toggleActive] = useState(false);
+  const overlayStyle = css`
+    background-color: rgba(255, 255, 255, 0.15);
+    height: 35rem;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    ${isActive ? 'background-color: rgba(255, 255, 255, 0)' : ''}
+  `;
+  const summaryStyle = css`
+    color: ${themeColors.white};
+    display: none;
+    margin: 2.5rem 3rem;
+    padding: 2rem 4rem;
+    position: absolute;
+    top: 10rem;
+    ${isActive ? 'display: inline;' : ''}
+  `;
+  return (
+    <Link
+      {...props}
+      to={`/submarkets/${submarket}`}
+      onFocus={(e) => toggleActive(true)}
+      onBlur={(e) => toggleActive(false)}
+      onMouseEnter={(e) => toggleActive(true)}
+      onMouseLeave={(e) => toggleActive(false)}
+    >
+      <div css={css`
+        ${imageStyle}
+        background-image: url(${image});
+      `}/>
+      <div css={overlayStyle} />
+      <h3 css={css`
+        ${h3Style}
         background-color: ${submarketColors[submarket]};
-    `}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    </p>
-  </Link>
-);
+      `}>
+        {`Submarket ${submarket}`}
+      </h3>
+      <p
+        css={css`
+          ${summaryStyle}
+          background-color: ${submarketColors[submarket]};
+      `}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Link>
+)};
 
 export default GridItem;
