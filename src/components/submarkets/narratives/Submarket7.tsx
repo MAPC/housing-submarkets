d600292f52b66c9125ce418c6893abb385b55b5d
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-02-17 11:02:40 -0500

    Add submarket 7 narrative'

--- src/components/submarkets/narratives/Submarket7.tsx
+++ src/components/submarkets/narratives/Submarket7.tsx
@@ -1,21 +1,58 @@
 import React from 'react';
 import SectionWithChart from '../SectionWithChart';
 import SectionWithoutChart from '../SectionWithoutChart';
+import PercentForeignBorn from '../visualizations/PercentForeignBorn';
+import PercentHouseholdsWithChildren from '../visualizations/PercentHouseholdsWithChildren'
+import PercentCostBurdened from '../visualizations/PercentCostBurdened';
 
 const SubmarketSevenNarrative = () => (
   <React.Fragment>
-    <SectionWithoutChart title="Offendit eleifend moderatius">
+    <SectionWithoutChart>
       <p>
-        Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Scripta periculis ei eam, te pro movet reformidans. Commune platonem mel id, brute adipiscing duo an. Scripta periculis ei eam, te pro movet reformidans. Usu ut commune mentitum, putent facete vim id. Quas scaevola postulant id vis, tincidunt assueverit et pro, quo et alii scripta accommodare. Te cum aeque repudiandae delicatissimi, cu populo dictas ponderum vel, dolor consequat ut vix. Scripta periculis ei eam, te pro movet reformidans. Sit fugit nostrum et. Scripta periculis ei eam, te pro movet reformidans.
+        Submarket 7 is the third largest by population, after Submarkets 3 and 4, and is home to 19 percent of people in the region. It is the Whitest of the submarkets, with White residents making up 87 percent of the population, nearly 20 points higher than the region overall. Black, Latinx, and Asian residents make up only two percent, three percent, and six percent of residents in the submarket, all well below their share regionwide (9,11,9). Despite their small share of the population, populations of color—especially Latinx and Asian populations—have been growing in the submarket since 2000. Asian residents grew by 154 percent and Latinx residents by 139 percent; Black residents grew by 57 percent. White residents grew by one percent in the submarket—slow growth, but growth, compared to their five percent decline in the region overall. Overall population growth in Submarket 7 has been average, at eight percent.
       </p>
     </SectionWithoutChart>
-    <SectionWithChart title="Lorem ipsum">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Soluta facilisi instructior eam in, ferri oratio ancillae te ius. Scripta periculis ei eam, te pro movet reformidans. Id doctus accommodare eam, pri an esse tota prodesset, te veniam oblique posidonium mel.
-      </p>
-      <p>
-        Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Scripta periculis ei eam, te pro movet reformidans. Commune platonem mel id, brute adipiscing duo an. Scripta periculis ei eam, te pro movet reformidans. Usu ut commune mentitum, putent facete vim id. Quas scaevola postulant id vis, tincidunt assueverit et pro, quo et alii scripta accommodare. Te cum aeque repudiandae delicatissimi, cu populo dictas ponderum vel, dolor consequat ut vix. Scripta periculis ei eam, te pro movet reformidans. Sit fugit nostrum et. Scripta periculis ei eam, te pro movet reformidans.
-      </p>
+    <SectionWithChart>
+      <div>
+        <p>
+          Immigrants are underrepresented in Submarket 7 compared to the region overall—11 percent of people in the submarket were born in another country, ten points lower than in the region overall, and the smallest share of all the submarkets. [BOX PLOT?] Only one percent of residents don’t speak English well.
+        </p>
+        <p>
+          Older adults are more likely to live in Submarket 7 than the region overall—32 percent of households have a member 65 years or older, four points higher than the region. Because of its large population share, that is more than 72,000 households, second highest in number after Submarket 4.
+        </p>
+      </div>
+      <PercentForeignBorn />
+    </SectionWithChart>
+    <SectionWithChart>
+      <div>
+        <p>
+          A lot of children live in Submarket 7 too. More households with children are located within Submarket 7 than any other submarket: 81,600, or 36 percent of all households. [BOX PLOT?]
+        </p>
+        <p>
+          Public school test scores in Submarket 7 are second best of the submarkets, after Submarket 5, as is the measure of student test score improvement.
+        </p>
+        <p>
+          The rate of college degree attainment in Submarket 7 is above the regional average by seven points at 59 percent, and is third highest of the region. Though, it trails behind Submarkets 1 and 5 by twenty points. Growth in college attainment has been just below the regional average, at 20 percent (compared to 24 percent).
+        </p>
+        <p>
+          As the lowest-density submarket and the farthest from the core of the region, it is not surprising that access to jobs via transit is lowest in Submarket 7.
+        </p>
+        <p>
+          Unemployment in the submarket, however, is below the regional average at 4.1 percent, and is second lowest after Submarket 5.
+        </p>
+      </div>
+      <PercentHouseholdsWithChildren />
+    </SectionWithChart>
+    <SectionWithChart>
+      <div>
+        <p>
+          Given the low cost of housing and the relative wealth of residents in Submarket 7, it is no surprise that the rate of severe cost burden in the submarket is lowest of the seven submarkets at 11 percent—5 points lower than the regional average. [BOX PLOT]
+        </p>
+        <p>
+          Redlining didn’t have much of an impact on Submarket 7 because in the 1930s not many people lived in the areas that far outside of the core of the region. Almost none of the land area in the submarket was assigned a HOLC grade.
+        </p>
+      </div>
+      <PercentCostBurdened />
     </SectionWithChart>
   </React.Fragment>
 );
---
+++ src/components/submarkets/visualizations/PercentForeignBorn.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { VegaLite, VisualizationSpec } from 'react-vega';
+import { StaticQuery, graphql } from "gatsby";
+
+const PercentForeignBorn = () => {
+  const spec: VisualizationSpec = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+    data: { name: "adjustedData" },
+    mark: {
+      type: "boxplot",
+      extent: "min-max",
+      size: 30,
+    },
+    height: 300,
+    width: 400,
+    encoding: {
+      x: {
+        field: "Submarket",
+        type: "nominal",
+      },
+      y: {
+        field: "fbP",
+        type: "quantitative",
+        title: "% Foreign Born Population",
+      },
+      color: {
+        legend: null,
+        field: "Submarket",
+        type: 'nominal',
+        scale: {
+          range: ['#C7024E', '#80CDC1', '#06614d', '#F37B74', '#BF812D', '#8C510A', '#6EA1C9'],
+          domain: [1,2,3,4,5,6,7],
+          zero: false,
+        }
+      },
+    }
+  };
+
+  return (
+    <StaticQuery query={
+      graphql`{
+        postgres {
+          allHousSubmarketsCtsList {
+            fbP
+            submktId
+          }
+        }
+      }`}
+      render={(data) => {
+        const adjustedData = data.postgres.allHousSubmarketsCtsList.map((row: { fbP: number, submktId: number }) => {
+          return {
+          'fbP': row.fbP,
+          'Submarket': row.submktId
+        }})
+        return (
+          <VegaLite spec={spec} data={{ adjustedData }} />
+        )
+      }}
+    />
+  )
+};
+
+export default PercentForeignBorn;
