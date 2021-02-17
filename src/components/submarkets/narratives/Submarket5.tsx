a0672c9760953031863ba4e838b88cfb11c2bcab
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-02-17 10:30:35 -0500

    Add submarket 5 narrative

--- src/components/submarkets/narratives/Submarket5.tsx
+++ src/components/submarkets/narratives/Submarket5.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import SectionWithChart from '../SectionWithChart';
 import SectionWithoutChart from '../SectionWithoutChart';
+import PercentHouseholdsWithChildren from '../visualizations/PercentHouseholdsWithChildren';
+import PercentCollegeAttainment from '../visualizations/PercentCollegeAttainment';
 
 const SubmarketFiveNarrative = () => (
   <React.Fragment>
-     <SectionWithChart title="Lorem ipsum">
+    <SectionWithoutChart>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Soluta facilisi instructior eam in, ferri oratio ancillae te ius. Scripta periculis ei eam, te pro movet reformidans. Id doctus accommodare eam, pri an esse tota prodesset, te veniam oblique posidonium mel.
+        Submarket 5 represents 9 percent of people in the region, a relatively small share compared to other submarkets. It also has the smallest share of Black and Latinx residents of all the submarkets—only two percent are Black and three percent are Latinx, compared to 9 percent and 11 percent, respectively, regionwide. Conversely, it has a larger share of Asian and White residents—12 percent Asian residents and 79 percent White residents, compared to nine percent and 68 percent regionwide. Despite the low numbers, Black and Latinx populations have grown in Submarket 5 since 2000. The Black population grew 45 percent and the Latinx population grew 135 percent. In total numbers, this represents only about 2,000 and 7,000 people, respectively. By comparison, the Asian population grew by 18,000 people, or 107 percent, over 2000. The White population declined at 6 percent (16,000 people), about the same pace as the region overall.
       </p>
       <p>
-        Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Scripta periculis ei eam, te pro movet reformidans. Commune platonem mel id, brute adipiscing duo an. Scripta periculis ei eam, te pro movet reformidans. Usu ut commune mentitum, putent facete vim id. Quas scaevola postulant id vis, tincidunt assueverit et pro, quo et alii scripta accommodare. Te cum aeque repudiandae delicatissimi, cu populo dictas ponderum vel, dolor consequat ut vix. Scripta periculis ei eam, te pro movet reformidans. Sit fugit nostrum et. Scripta periculis ei eam, te pro movet reformidans.
+        Submarket 5 has a relatively large share of households with members ages 65 or older—35 percent compared to 29 percent in the region overall. Because of its small population size, however, that represents just 10 percent of households with seniors in the region.
       </p>
+    </SectionWithoutChart>
+    <SectionWithChart>
+      <div>
+        <p>
+          Submarket 5 has the largest share of households with children of all the submarkets at 39 percent, which is ten points higher than the region overall. [BOX PLOT] It also has an outsized share of households with children compared to its size. However, because it is a relatively small submarket, the total number of households with children is just under 42,000, or only 11 percent of all households with children in the region.
+        </p>
+        <p>
+          It is no wonder that Submarket 5 has a high share of households with children, because it has the best public school performance of the seven submarkets. MCAS scores for both math and English and language arts are the highest, as are the measures of student improvement from third to eighth grade. Yet, only a small share of children in the region are able to access these high-performing school systems, and an even smaller share of Black and Latinx children.
+        </p>
+      </div>
+      <PercentHouseholdsWithChildren />
+    </SectionWithChart>
+    <SectionWithChart>
+      <div>
+        <p>
+          It is no wonder that the rate of college attainment is highest in Submarket 7, at 79 percent—27 points higher than the regional average. [BOX PLOT]
+        </p>
+        <p>
+          Workers in Submarket 5 have the lowest rate of unemployment of the submarkets, at 3.7 percent.
+        </p>
+        <p>
+          Despite the high home prices in Submarket 5, the rate of severe cost burden is low (13 percent) compared to the region overall (16 percent).
+        </p>
+      </div>
+      <PercentCollegeAttainment />
     </SectionWithChart>
   </React.Fragment>
 );
---
+++ src/components/submarkets/visualizations/PercentHouseholdsWithChildren.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { VegaLite, VisualizationSpec } from 'react-vega';
+import { StaticQuery, graphql } from "gatsby";
+
+const PercentHouseholdsWithChildren = () => {
+  const spec: VisualizationSpec = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+    data: { name: "adjustedData" },
+    mark: {
+      type: "boxplot",
+      extent: "min-max",
+      size: 30,
+    },
+    height: 300,
+    width: 400,
+    encoding: {
+      x: {
+        field: "Submarket",
+        type: "nominal",
+      },
+      y: {
+        field: "hhkidsP",
+        type: "quantitative",
+        title: "% Households with Children",
+      },
+      color: {
+        legend: null,
+        field: "Submarket",
+        type: 'nominal',
+        scale: {
+          range: ['#C7024E', '#80CDC1', '#06614d', '#F37B74', '#BF812D', '#8C510A', '#6EA1C9'],
+          domain: [1,2,3,4,5,6,7]
+        }
+      },
+    }
+  };
+
+  return (
+    <StaticQuery query={
+      graphql`{
+        postgres {
+          allHousSubmarketsCtsList {
+            hhkidsP
+            submktId
+          }
+        }
+      }`}
+      render={(data) => {
+        const adjustedData = data.postgres.allHousSubmarketsCtsList.map((row: { hhkidsP: number, submktId: number }) => {
+          return {
+          'hhkidsP': row.hhkidsP,
+          'Submarket': row.submktId
+        }})
+        return (
+          <VegaLite spec={spec} data={{ adjustedData }} />
+        )
+      }}
+    />
+  )
+};
+
+export default PercentHouseholdsWithChildren;
