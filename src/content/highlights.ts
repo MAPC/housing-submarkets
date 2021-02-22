61bea6b44d534ce16a562e1b462e8f65566ef05d
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-02-22 15:46:39 -0500

    Switch text placement for homepage info card

--- src/content/highlights.ts
+++ src/content/highlights.ts
@@ -2,27 +2,39 @@ type Submarket = 1 | 2 | 3 | 4 | 5 | 6 | 7;
 
 const submarketHighlights: {[Key in Submarket]: Array<string> } = {
   1: [
-    "Racially and ethnically diverse, but with declining Black and growing White population",
-    "Mostly younger adults without children",
-    "High rate of college education",
-    "Above average rate of severe cost burden"
+    "Premium-priced",
+    "Increasing home prices and rents",
+    "Declining numbers of renters"
   ],
   2: [
-    "Racially and ethnically diverse with two-thirds residents of color",
-    "Highest unemployment rate",
-    "Highest rate of severe cost burden",
-    "Highest home mortgage denial rate",
+    "High-density, lower-cost urban markets",
+    "High rates of foreclosure and cash sales",
+    "Rapid sale price escalation",
   ],
-  3: [],
-  4: [],
-  5: [],
+  3: [
+    "Oldest housing stock",
+    "Mix of single-family and small multifamily",
+    "Increasing home prices and rents"
+  ],
+  4: [
+    "Older housing stock in suburbs and regional urban centers",
+    "Low density and housing costs",
+    "Moderate incomes, minimal change"
+  ],
+  5: [
+    "Premium-priced low-density suburbs",
+    "Large, older housing stock in very expensive single-family home neighborhoods"
+  ],
   6: [
-    "Like the region in many ways",
-    "Racially and ethnically diverse",
-    "Fastest growth of Black, Latinx, and Asian populations",
-    "High public school growth performance",
+    "Suburban edges and new urban centers",
+    "Newest housing stock and lowest home prices",
+    "Rapid increases in density, rents, and renters"
   ],
-  7: []
+  7: [
+    "Low-density, moderate-cost single-family suburbs",
+    "Newer housing stock",
+    "Few renters"
+  ]
 }
 
 export { submarketHighlights };
